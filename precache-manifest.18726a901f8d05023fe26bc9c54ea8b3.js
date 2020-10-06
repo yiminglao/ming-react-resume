ab1ec2d20398cdafe4c9e276d7d827b261980489
ming <yiming.mp@gmail.com>
2020-10-06 05:33:34 -0600

    Updates

---
+++ precache-manifest.18726a901f8d05023fe26bc9c54ea8b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb3aefade8d2c9b0ddcef9a96c061eb1",
+    "url": "/yiminglao/ming-react-resume/index.html"
+  },
+  {
+    "revision": "d598a539ad690ed2e5f5",
+    "url": "/yiminglao/ming-react-resume/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "af6b69830d08eef60713",
+    "url": "/yiminglao/ming-react-resume/static/js/2.2fd55067.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/yiminglao/ming-react-resume/static/js/2.2fd55067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d598a539ad690ed2e5f5",
+    "url": "/yiminglao/ming-react-resume/static/js/main.22e17583.chunk.js"
+  },
+  {
+    "revision": "44fd175785f7519c1f5f",
+    "url": "/yiminglao/ming-react-resume/static/js/runtime-main.7bc948bb.js"
+  }
+]);
