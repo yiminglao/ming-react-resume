ffb12dc851c5324ce477a81991982852e50df91d
ming <yiming.mp@gmail.com>
2020-10-07 04:48:47 -0600

    Updates

---
+++ precache-manifest.cc713a80956972e1a7b70d10482f1f5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf2b4f134b65924d7e327ddb166b1c0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c280e8f51b03cb5cab8c",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "af6b69830d08eef60713",
+    "url": "./static/js/2.2fd55067.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.2fd55067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c280e8f51b03cb5cab8c",
+    "url": "./static/js/main.0aa06f4d.chunk.js"
+  },
+  {
+    "revision": "dbab036f87ca1350243a",
+    "url": "./static/js/runtime-main.851de3a6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ming-react-resume/precache-manifest.793307707edce9352785339f2696b6a8.js"
+  "./precache-manifest.cc713a80956972e1a7b70d10482f1f5e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ming-react-resume/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
